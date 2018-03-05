30955adfe818d5da2b11bec87f5387bbc3b9bfa1
Thanh Van <tthanh.smt@gmail.com>
2018-03-05 16:03:23 +0700

    go: update gigasecond exercise

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -7,5 +7,5 @@ import (
 
 // AddGigasecond caculate the moment when someone has lived for 10^9 seconds.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(1000000000 * time.Second)
+	return t.Add(1e9 * time.Second)
 }
