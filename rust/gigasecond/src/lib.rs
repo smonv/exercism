dbb3ae60a8c8ab6d04b4403504043055279b2d14
Thanh Van <thanh.van@smonv.io>
2018-12-19 21:55:10 +0700

    rust: update

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+use chrono::{DateTime, Duration, Utc};
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    let sec = Duration::seconds(1_000_000_000);
+    start + sec
+}
